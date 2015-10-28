71e719e068c67f2247f08494d84699526cec97ce
oOLokiOo <ivan.volkov.older@gmail.com>
2015-10-28 05:11:16 +0300

    prepare for GUI GOLANG version

---
+++ versions/golang/gui/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"time"
+
+	"github.com/google/gxui"
+	"github.com/google/gxui/drivers/gl"
+	"github.com/google/gxui/gxfont"
+	"github.com/google/gxui/math"
+	"github.com/google/gxui/samples/flags"
+)
+
+func main() {
+
+}
