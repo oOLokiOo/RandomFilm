536e330ff997ab903ee44641501ba2227871c50a
oOLokiOo <ivan.volkov.older@gmail.com>
2015-10-28 06:16:06 +0300

    prepare for GUI GOLANG version

--- versions/golang/gui/main.go
+++ versions/golang/gui/main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
-	"time"
-
 	"github.com/google/gxui"
 	"github.com/google/gxui/drivers/gl"
 	"github.com/google/gxui/gxfont"
-	"github.com/google/gxui/math"
 	"github.com/google/gxui/samples/flags"
 )
 
-func main() {
+func appMain(driver gxui.Driver) {
+	theme := flags.CreateTheme(driver)
 
+	font, err := driver.CreateFont(gxfont.Default, 75)
+	if err != nil {
+		panic(err)
+	}
+
+	window := theme.CreateWindow(800, 600, "Random movie that you would like to revise (c) Script by oOLokiOo")
+	window.SetBackgroundBrush(gxui.CreateBrush(gxui.Gray50))
+
+	label := theme.CreateLabel()
+	label.SetFont(font)
+	label.SetText("just test")
+
+	window.AddChild(label)
+
+	window.OnClose(driver.Terminate)
+}
+
+func main() {
+	gl.StartDriver(appMain)
 }
