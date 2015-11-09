0161873e273ea2a6013a4558995a27644ae9c603
oOLokiOo <ivan.volkov.older@gmail.com>
2015-11-09 18:55:57 +0300

    add project XML file structure to comment

--- versions/php/new-version/index.php
+++ versions/php/new-version/index.php
@@ -6,6 +6,27 @@ ini_set('error_reporting', E_ALL);
 
 require_once getcwd().'/inc/app.class.php';
 
+/* *** PROJECT XML STRUCTURE ***
+
+<?xml version="1.0" encoding="UTF-8"?>
+<movies>
+	<film>
+		<ru>test movie</ru>
+		<en>тест</en>
+		<year>1234</year>
+		<kinopoisk>http://www.kinopoisk.ru/xxx</kinopoisk>
+		<imdb>http://www.imdb.com/xxx</imdb>
+	</film>
+	<film>
+		<ru>test movie 2</ru>
+		<en>тест</en>
+		<year>1234</year>
+		<kinopoisk>http://www.kinopoisk.ru/xxx2</kinopoisk>
+		<imdb>http://www.imdb.com/xxx2</imdb>
+	</film>
+</movies>
+*/
+
 $XML_PATH = '../../../users/1/films.xml';
 $CSS_PATH = '../../../../css/style.css';
 
