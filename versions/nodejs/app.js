ce545e129abe56d41a500db2006ac565d843abd1
Ivan Volkov <ivan.volkov.older@gmail.com>
2018-02-08 05:27:00 +0300

    add xml2js lib & reading user xml

--- versions/nodejs/app.js
+++ versions/nodejs/app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var http 	= require("http");
 //var ejs 	= require('ejs');
 //var path	= require('path');
+var fs 		= require('fs');
+var xml2js 	= require('xml2js');
 
 
 // Config
@@ -16,10 +18,13 @@ var film = {};
 
 // Start server - Express
 var app = express();
+var parser = new xml2js.Parser();
+
+
 app.set("port", 8080);
+app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
-app.set('view engine', 'ejs');
 
 http.createServer(app).listen(app.get("port"), function(){
 	console.log("Express server listening on port " + app.get("port"));
@@ -45,4 +50,16 @@ app.use(function(req, res, next) {
 
 app.use(function(req, res) {
 	res.send(404, res.render('pages/404'));
+});
+
+
+// Get user XML
+var user_xml_path = "/public/users/1/films.xml";
+fs.readFile(__dirname + user_xml_path, function(err, data) {
+	parser.parseString(data, function (err, result) {
+		console.dir(result.movies["film"][0].ru);
+		console.dir(result.movies["film"][0].en);
+		console.dir(result.movies["film"][0].kinopoisk);
+		console.log("Done!");
+	});
 });
